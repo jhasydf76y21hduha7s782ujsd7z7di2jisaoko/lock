f704aaba466b7a857ed2956fd866ab765619f8ba
menaki1337 <126506517+menaki1337@users.noreply.github.com>
2023-03-10 19:14:23 +0200

    Create streamable.lua

---
+++ streamable.lua
@@ -0,0 +1,48 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ChaseSYNX/Scripts/main/Streamable-Silent"))()
+DaHoodSettings.Prediction = 0.121
+Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "HumanoidRootPart", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso"}
+Aiming.FOV = 30
+Aiming.FOVSides = 360
+Aiming.HitChance = 124
+Aiming.ShowFOV = false
+
+wait(2)
+
+getgenv().Fatality = {
+    Underground = {
+        ["Enabled"] = true,
+        ["Amount"] = 150,
+        ["Keybind"] = "",
+    },
+    Sky = {
+        ["Enabled"] = true,
+        ["Amount"] = 150,
+        ["Keybind"] = "Z",
+    },
+    CustomVelocity = {
+        ["Enabled"] = true,
+        ["CustomX"] = 200,
+        ["CustomY"] = 200,
+        ["CustomZ"] = 200,
+        ["Keybind"] = "",
+    },
+    Shake = {
+        ["Enabled"] = true,
+        ["Keybind"] = "",
+    },
+    AimViewer = {
+        ["Enabled"] = true,
+        ["ViewTarget"] = false,
+        ["RainbowBeam"] = false,
+        ["Keybind"] = "T",
+        ["Width"] = 0.1,
+        ["Color"] = Color3.fromRGB(255, 255, 255),
+    },
+    Miscellaneous = {
+        ["VelocityStatistics"] = false
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tailgater/Fatality/main/StandaloneAntiLock", true))()
+
+end
